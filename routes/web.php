61270769498006de7ba98d381c7868155de4af05
Startang141 <bintangadiyatma141@gmail.com>
2022-02-23 14:00:49 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,20 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Praktikum 1
-Route::get('/', function () {
-    echo "Hi! Selamat Datang di Website Laravel";
-});
+// //Praktikum 1
+// // URL : /
+// Route::get('/', function () {
+//     echo "Hi! Selamat Datang di Website Laravel";
+// });
+// // URL : /about
+// Route::get('/about', function () {
+//     echo "
+//     NIM     : 2041720130
+//     <br>
+//     Nama    : Bintang Adiyatma Agung Putra
+//     <br>
+//     Kelas   : TI-2G";
+// });
+// // URL : /articles/{id}
+// Route::get('/articles/{id}', function ($id) {
+//     echo "Ini adalah halaman Artikel dengan ID " . $id;
+// });
 
-Route::get('/about', function () {
-    echo "
-    NIM     : 2041720130
-    <br>
-    Nama    : Bintang Adiyatma Agung Putra
-    <br>
-    Kelas   : TI-2G";
-});
-
-Route::get('/articles/{id}', function ($id) {
-    echo "Ini adalah halaman Artikel dengan ID " . $id;
-});
+//Praktikum 2
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
